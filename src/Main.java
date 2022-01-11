08b3b08a6e579b4bee5c03c21d90a2586c2700c5
BhavinThakkar18 <bhavin.majethia18@gmail.com>
2022-01-11 16:00:33 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[])
+    {
+        Student studentA = new Student("Bhavin","Majithia",2020,3.67,"Computer Engineering");
+        Student studentB = new Student("Shyam","Joshi",2020,4.1,"Computer Engineering");
+        System.out.println(studentB.gpa);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String firstName;
+    String lastName;
+    int expectedYearToGraduate;
+    double gpa;
+    String declaredMajor;
+
+    public Student(String firstName,String lastName, int expectedYearToGraduate,double gpa, String declaredMajor)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.expectedYearToGraduate=expectedYearToGraduate;
+        this.gpa=gpa;
+        this.declaredMajor=declaredMajor;
+
+    }
+}
